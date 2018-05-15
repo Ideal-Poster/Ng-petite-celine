61a47d98c9428f0dffdb252d46acf075d49c36e5
Malcolm Gourdine <malcolmgourdine@gmail.com>
2018-05-15 19:49:14 -0400

    mdbootstrap tweek

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 // import { Observable } from 'rxjs';
-import { initializeApp, database } from 'firebase';
+import {
+  initializeApp,
+  database
+} from 'firebase';
 
-import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
-import { MailChimpApiService } from './services/mail-chimp-api.service';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from 'angularfire2/firestore';
+import {
+  MailChimpApiService
+} from './services/mail-chimp-api.service';
 
 @Component({
   selector: 'app-root',
@@ -16,4 +26,51 @@ export class AppComponent {
   constructor(db: AngularFirestore) {
 
   }
+  imagesBasic = [{
+      img: 'https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?w=640&ssl=1',
+      thumb: 'https://i2.wp.com/beebom.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg?w=640&ssl=1',
+      description: 'Image 1'
+    },
+    {
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg',
+      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg',
+      description: 'Image 2'
+    },
+    {
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(131).jpg',
+      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(131).jpg',
+      description: 'Image 3'
+    },
+    {
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(123).jpg',
+      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(123).jpg',
+      description: 'Image 4'
+    },
+    {
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(118).jpg',
+      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(118).jpg',
+      description: 'Image 5'
+    },
+    {
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(128).jpg',
+      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(128).jpg',
+      description: 'Image 6'
+    },
+    {
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(132).jpg',
+      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(132).jpg',
+      description: 'Image 7'
+    },
+    {
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(115).jpg',
+      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(115).jpg',
+      description: 'Image 8'
+    },
+    {
+      img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg',
+      thumb: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(133).jpg',
+      description: 'Image 9'
+    }
+  ];
+
 }
