4eb589ecb3998954e080aa985c72cfc262021fff
Malcolm Gourdine <malcolmgourdine@gmail.com>
2018-05-21 17:22:50 -0400

    actress page style

--- src/app/acting-page/acting-page.component.ts
+++ src/app/acting-page/acting-page.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./acting-page.component.css']
 })
 export class ActingPageComponent implements OnInit {
+  mask1 = true;
+  mask2 = true;
+  mask3 = true;
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
+    this.mask1 = false;
+    this.mask2 = false;
+    this.mask3 = false;
+    setTimeout(() => {
+      this.mask1 = true;
+      this.mask2 = true;
+      this.mask3 = true;
+    }, 20);
+  }
+
+  toggleMask(mask: boolean, index: number) {
+    mask === true && index === 1 ? this.mask1 = false : this.mask1 = true;
+    mask === true && index === 2 ? this.mask2 = false : this.mask2 = true;
+    mask === true && index === 3 ? this.mask3 = false : this.mask3 = true;
+  }
+
+  private toggleAll() {
+
   }
 
 }
