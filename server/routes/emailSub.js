f1b49e0f78acc4e05b5e5cc234cdeaa8405b0453
Malcolm Gourdine <malcolmgourdine@gmail.com>
2018-06-30 22:38:45 -0400

    deploy

--- server/routes/emailSub.js
+++ server/routes/emailSub.js
@@ -2,22 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
-  // var request = require("request");
-  // var options = { method: 'POST',
-  //   url: '',
-  //   qs: { u: '4c131a3932ca2dd768c590f9b', 'amp;id': 'c760b1311e' },
-  //   headers:
-  //    { 'Postman-Token': '227ad449-10fb-4d78-acc0-de5c000f408c',
-  //      'Cache-Control': 'no-cache',
-  //      'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
-  //   formData: { NAME: req.body.name, EMAIL: req.body.email } };
-  //   // https://petiteceline.us17.list-manage.com/subscribe/post
-  // request( function (error, response, body) {
-  //   if (error) throw new Error(error);
+  var request = require("request");
 
-  //   console.log(body);
-  // });
-  // console.log(req.body.name);
+  var options = { method: 'POST',
+    url: 'https://petiteceline.us17.list-manage.com/subscribe/post',
+    qs: { u: '4c131a3932ca2dd768c590f9b', 'amp;id': 'c760b1311e' },
+    headers:
+     { 'Postman-Token': '77655193-3f8f-4032-9ab6-9231c22f20c5',
+       'Cache-Control': 'no-cache',
+       Authorization: 'Basic UGV0aXRlIENlbGluZTo5NGU1YzM2MzIxNmZiMTU3ZWZjZDgzNzk0ODcwZmE1YS11czE3',
+       'Content-Type': 'application/x-www-form-urlencoded',
+       'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
+    formData: { NAME: 'hellokitty', EMAIL: 'malcolmgourdine@gmail.com' } };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+  });
+
 
   res.status(201).json({
     message: 'Post added successfully'
