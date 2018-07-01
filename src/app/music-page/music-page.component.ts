30c02bc92756d9d21fc21a09a31989603345d1a8
Malcolm Gourdine <malcolmgourdine@gmail.com>
2018-06-30 22:50:45 -0400

    deploy

--- src/app/music-page/music-page.component.ts
+++ src/app/music-page/music-page.component.ts
@@ -47,7 +47,7 @@ export class MusicPageComponent implements OnInit {
 
     console.log(this.signupForm.value.userData);
     this.http
-      .post<{ message: string }>('http://localhost:3000/emailSub', {
+      .post<{ message: string }>('https://floating-earth-31375.herokuapp.com/', {
         name: this.signupForm.value.userData.name,
         email: this.signupForm.value.userData.email
         }
