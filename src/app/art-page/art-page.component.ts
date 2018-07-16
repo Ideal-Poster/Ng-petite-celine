3cf6e26df1af6bca82862e155b286c3c56ba9463
Malcolm Gourdine <malcolmgourdine@gmail.com>
2018-07-16 17:56:03 -0400

    lightbox mobile view fixed

--- src/app/art-page/art-page.component.ts
+++ src/app/art-page/art-page.component.ts
@@ -37,46 +37,127 @@ import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 
 export class ArtPageComponent {
   title = 'Art';
-  imagesBasic = [{
-      img: '../../assets/gallery-images/photo-1.JPG',
-      thumb: '../../assets/gallery-images/photo-1.JPG',
+  imagesBasic = [
+    {
+      img: '../../assets/gallery-images/Tulips - Light.jpg',
+      thumb: '../../assets/gallery-images/Tulips - Light.jpg',
       description: 'Image 1'
     },
     {
-      img: '../../assets/gallery-images/photo-2.JPG',
-      thumb: '../../assets/gallery-images/photo-2.JPG',
+      img: '../../assets/gallery-images/Tulips.jpg',
+      thumb: '../../assets/gallery-images/Tulips.jpg',
       description: 'Image 2'
     },
     {
-      img: '../../assets/gallery-images/photo-3.JPG',
-      thumb: '../../assets/gallery-images/photo-3.JPG',
+      img: '../../assets/gallery-images/Green Invasion - Light Only.jpg',
+      thumb: '../../assets/gallery-images/Green Invasion - Light Only.jpg',
       description: 'Image 3'
     },
     {
-      img: '../../assets/gallery-images/photo-4.JPG',
-      thumb: '../../assets/gallery-images/photo-4.JPG',
+      img: '../../assets/gallery-images/Green Invasion - Light.jpg',
+      thumb: '../../assets/gallery-images/Green Invasion - Light.jpg',
       description: 'Image 4'
     },
     {
-      img: '../../assets/gallery-images/photo-5.JPG',
-      thumb: '../../assets/gallery-images/photo-5.JPG',
+      img: '../../assets/gallery-images/Green Invasion.jpg',
+      thumb: '../../assets/gallery-images/Green Invasion.jpg',
       description: 'Image 5'
     },
     {
-      img: '../../assets/gallery-images/photo-6.JPG',
-      thumb: '../../assets/gallery-images/photo-6.JPG',
+      img: '../../assets/gallery-images/GypsyLynx - Light Only.jpg',
+      thumb: '../../assets/gallery-images/GypsyLynx - Light Only.jpg',
       description: 'Image 6'
     },
     {
-      img: '../../assets/gallery-images/photo-7.JPG',
-      thumb: '../../assets/gallery-images/photo-7.JPG',
+      img: '../../assets/gallery-images/GypsyLynx - Light.jpg',
+      thumb: '../../assets/gallery-images/GypsyLynx - Light.jpg',
       description: 'Image 7'
     },
     {
-      img: '../../assets/gallery-images/photo-8.JPG',
-      thumb: '../../assets/gallery-images/photo-8.JPG',
+      img: '../../assets/gallery-images/GypsyLynx in Smoke -Light.jpg',
+      thumb: '../../assets/gallery-images/GypsyLynx in Smoke -Light.jpg',
       description: 'Image 8'
-    }
+    },
+    {
+      img: '../../assets/gallery-images/GypsyLynx in Smoke- Light Only.jpg',
+      thumb: '../../assets/gallery-images/GypsyLynx in Smoke- Light Only.jpg',
+      description: 'Image 9'
+    },
+    {
+      img: '../../assets/gallery-images/GypsyLynx in Smoke.jpg',
+      thumb: '../../assets/gallery-images/GypsyLynx in Smoke.jpg',
+      description: 'Image 10'
+    },
+       {
+      img: '../../assets/gallery-images/GypsyLynx in Space - Light Only.jpg',
+      thumb: '../../assets/gallery-images/GypsyLynx in Space - Light Only.jpg',
+      description: 'Image 11'
+    },
+    {
+      img: '../../assets/gallery-images/GypsyLynx in Space - Light.jpg',
+      thumb: '../../assets/gallery-images/GypsyLynx in Space - Light.jpg',
+      description: 'Image 12'
+    },
+    {
+      img: '../../assets/gallery-images/GypsyLynx in Space.jpg',
+      thumb: '../../assets/gallery-images/GypsyLynx in Space.jpg',
+      description: 'Image 13'
+    },
+    {
+      img: '../../assets/gallery-images/GypsyLynx.jpg',
+      thumb: '../../assets/gallery-images/GypsyLynx.jpg',
+      description: 'Image 14'
+    },
+    {
+      img: '../../assets/gallery-images/Healing Hands - Glow Only.jpg',
+      thumb: '../../assets/gallery-images/Healing Hands - Glow Only.jpg',
+      description: 'Image 15'
+    },
+    {
+      img: '../../assets/gallery-images/Healing Hands - Light Only.jpg',
+      thumb: '../../assets/gallery-images/Healing Hands - Light Only.jpg',
+      description: 'Image 16'
+    },
+       {
+      img: '../../assets/gallery-images/Healing Hands - Light.jpg',
+      thumb: '../../assets/gallery-images/Healing Hands - Light.jpg',
+      description: 'Image 17'
+    },
+    {
+      img: '../../assets/gallery-images/Healing Hands.jpg',
+      thumb: '../../assets/gallery-images/Healing Hands.jpg',
+      description: 'Image 18'
+    },
+    {
+      img: '../../assets/gallery-images/Portal - Light Only.jpg',
+      thumb: '../../assets/gallery-images/Portal - Light Only.jpg',
+      description: 'Image 19'
+    },
+    {
+      img: '../../assets/gallery-images/Portal - Light.jpg',
+      thumb: '../../assets/gallery-images/Portal - Light.jpg',
+      description: 'Image 20'
+    },
+    {
+      img: '../../assets/gallery-images/Portal.jpg',
+      thumb: '../../assets/gallery-images/Portal.jpg',
+      description: 'Image 21'
+    },
+       {
+      img: '../../assets/gallery-images/Tulips - Light Only_.jpg',
+      thumb: '../../assets/gallery-images/Tulips - Light Only_.jpg',
+      description: 'Image 22'
+    },
+    {
+      img: '../../assets/gallery-images/Tulips - Light.jpg',
+      thumb: '../../assets/gallery-images/Tulips - Light.jpg',
+      description: 'Image 23'
+    },
+    {
+      img: '../../assets/gallery-images/Tulips.jpg',
+      thumb: '../../assets/gallery-images/Tulips.jpg',
+      description: 'Image 24'
+    },
   ];
 
   @ViewChild('lightbox') public el: any;
