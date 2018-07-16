ee42d1ebc277df1a254a0f6247e35b36ecb68ffb
Malcolm Gourdine <malcolmgourdine@gmail.com>
2018-07-16 11:36:14 -0400

    email post location change

--- src/app/music-page/music-page.component.ts
+++ src/app/music-page/music-page.component.ts
@@ -48,7 +48,7 @@ export class MusicPageComponent implements OnInit {
 
     console.log(this.signupForm.value.userData);
     this.http
-      .post<{ message: string }>('www.petiteceline.com/emailSub', {
+      .post<{ message: string }>('/emailSub', {
         name: this.signupForm.value.userData.name,
         email: this.signupForm.value.userData.email
         }
